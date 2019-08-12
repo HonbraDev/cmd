891788c26a67111c9f8d13674842a3328b37dd07
HonbraOfficial <HonbraOfficial@users.noreply.github.com>
2019-08-12 14:27:23 +0200

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,44 +1,5 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvn1aSaKe0a8ECqd5c5tg==
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvnyqaHLvQW+AvmdIJN
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvnzbGMbuIS/iU=
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvn36yHNK4R61GE
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvn0rGCMK4R61GE
-::fBE1pAF6MU+EWHreyHcjLQlHcCyNMmmoB4k44fvwr82IrUoRUd48dYvn0rGCMK4H8lGE
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdCyDJGyX8VAjFDNbQgaQPVeeCaIS5Of66/m7oUAZGus8d+8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF65
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAjk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDNbQgaQPVeeCaIS5Of66/m7ikITVvA+P5yb9aSaKe0a8ED2NcdjhjRfgM5s
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
-set v=3.0
+set v=3.0 GitHub
 cd "%userprofile%\Disk Google\cmd"
 :start
 mode con:cols=35 lines=3
