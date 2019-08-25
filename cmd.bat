be8cc2eed61987e23bbc7fdb929f50f2a783188c
HonbraOfficial <HonbraOfficial@users.noreply.github.com>
2019-08-25 17:17:55 +0200

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -5,6 +5,28 @@ set v=3.1 GitHub Release
 title Honbra's CMD %v%
 cls
 echo.
+echo                                                          .oys-                                      
+echo                                                         /NMMMM                                      
+echo              `:oyhdddddddddddddddddddy``odmy.         .hMMMMMM` /hddddddddddddddddhso:`             
+echo           .omMMMMMMMMMMMMMMMMMMMMMMMo :mMMMMs        +NMMMMMMM`.MMMMMMMMMMMMMMMMMMMMMMMdo.          
+echo         -hMMMMMMMMMMMMMMMMMMMMMMMMm-`sMMMMMMs      .hMMMMMMMMM`.MMMMMMMMMMMMMMMMMMMMMMMMMMh-        
+echo        oMMMMMMMMMMMMMMMMMMMMMMMMMo :mMMMMMMMs     +NMMMMMMMMMM`.MMMMMMMMMMMMMMMMMMMMMMMMMMMMo       
+echo       yMMMMMMMMMMMMMMMMMMMMMMMNh-`sMMMMMMMMMs   .hMMMMMMMMMMMM`.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMy      
+echo      /MMMMMMMMMMy:`             :mMMMMMMMMMMs  +NMMMMMMMMMMMMM`.MMMMMMMMMs       `:yMMMMMMMMMM/     
+echo      mMMMMMMMMM-              `sMMMMMMMMMMMMs.hMMMMMMMMMMMMMMM`.MMMMMMMMMs          -NMMMMMMMMm     
+echo      MMMMMMMMMh              :mMMMMMMMMMMMMMmNMMMMMMMMMMMMMMMM`.MMMMMMMMMs           hMMMMMMMMM     
+echo      mMMMMMMMMN.           `yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`.MMMMMMMMMs          .NMMMMMMMMm     
+echo      oMMMMMMMMMN+`        :mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`.MMMMMMMMMs        `+NMMMMMMMMMo     
+echo       dMMMMMMMMMMMmdddddddMMMMMMMMMMMMMMMMMMMMMMMMMm/MMMMMMMMMddMMMMMMMMMNdddddddmMMMMMMMMMMMd      
+echo       `hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs``MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh`      
+echo         /mMMMMMMMMMMMMMMMMMMMMMMMN+NMMMMMMMMMMMMm:  `MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/        
+echo           /hMMMMMMMMMMMMMMMMMMMMh. NMMMMMMMMMMMs`   `MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh/          
+echo             `:oydNMMMMMMMMMMMMm+   NMMMMMMMMMm:      sNMMMMMMMMMMMMMMMMMMMMMMMMMMMNdyo:`            
+echo                                    NMMMMMMMMs`                                                      
+echo                                    NMMMMMMm:                                                        
+echo                                    NMMMMMs`                                                         
+echo                                    dMMMm:                                                           
+echo                                    `/+:                                                             
 set h=null
 set /p h="> "
 if exist "%h%.bat" "%h%.bat"
